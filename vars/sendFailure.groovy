0899d853e40b9caafea081d2ee305e818c811b5d
Dennis W. Otugo <otugodennis@gmail.com>
2018-10-24 09:47:16 +0100

    Create sendFailure.groovy

---
+++ vars/sendFailure.groovy
@@ -0,0 +1,134 @@
+#!/usr/bin/env groovy
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import hudson.tasks.test.AbstractTestResultAction;
+import hudson.model.Actionable;
+
+def call(String buildStatus = 'STARTED', String channel = '#jenkins') {
+
+  // buildStatus of null means successfull
+  buildStatus = buildStatus ?: 'SUCCESSFUL'
+  channel = channel ?: '#jenkins'
+  def jobName = "${env.JOB_NAME}"
+  jobName = jobName.getAt(0..(jobName.indexOf('/') - 1))
+  // Default values
+  def colorName = 'RED'
+  def colorCode = '#FF0000'
+  def subject = "${buildStatus}: Build [${env.BUILD_NUMBER}] (<${env.RUN_DISPLAY_URL}|Open>) (<${env.RUN_CHANGES_DISPLAY_URL}|  Changes>)"
+  def title = "${jobName}"
+  def title_link = "${env.RUN_DISPLAY_URL}"
+  def branchName = "${env.BRANCH_NAME}"
+
+  def commit = sh(returnStdout: true, script: 'git rev-parse HEAD')
+  def author = sh(returnStdout: true, script: "git --no-pager show -s --format='%an'").trim()
+
+  def message = sh(returnStdout: true, script: 'git log -1 --pretty=%B').trim()
+
+  // Override default values based on build status
+  if (buildStatus == 'STARTED') {
+    color = 'YELLOW'
+    colorCode = '#FFFF00'
+  } else if (buildStatus == 'SUCCESSFUL') {
+    color = 'GREEN'
+    colorCode = 'good'
+  } else if (buildStatus == 'UNSTABLE') {
+    color = 'YELLOW'
+    colorCode = 'warning'
+  } else {
+    color = 'RED'
+    colorCode = 'danger'
+  }
+
+  // get test results for slack message
+  @NonCPS
+  def getTestSummary = { ->
+    def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
+    def summary = ""
+
+    if (testResultAction != null) {
+        def total = testResultAction.getTotalCount()
+        def failed = testResultAction.getFailCount()
+        def skipped = testResultAction.getSkipCount()
+
+        summary = "Test results:\n\t"
+        summary = summary + ("Passed: " + (total - failed - skipped))
+        summary = summary + (", Failed: " + failed + " ${testResultAction.failureDiffString}")
+        summary = summary + (", Skipped: " + skipped)
+    } else {
+        summary = "No tests found"
+    }
+    return summary
+  }
+  
+  @NonCPS
+  def getFailedTests = { ->
+      def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
+      def failedTestsString = "```"
+
+      if (testResultAction != null) {
+          def failedTests = testResultAction.getFailedTests()
+
+          if (failedTests.size() > 9) {
+              failedTests = failedTests.subList(0, 8)
+          }
+
+          for(CaseResult cr : failedTests) {
+              failedTestsString = failedTestsString + "${cr.getFullDisplayName()}:\n${cr.getErrorDetails()}\n\n"
+          }
+          failedTestsString = failedTestsString + "```"
+      }
+      return failedTestsString
+  }
+  
+  def failSummaryRaw = getFailedTests()
+  def testSummaryRaw = getTestSummary()
+  // format test summary as a code block
+  def failedSummary = "```${failSummaryRaw}```"
+  println failedSummary.toString()
+  def testSummary = "```${testSummaryRaw}```"
+  println testSummary.toString()
+
+  JSONObject attachment = new JSONObject();
+  attachment.put('author',"jenkins");
+  attachment.put('author_link',"https://build.ecms-stage.gloo.ng");
+  attachment.put('title', title.toString());
+  attachment.put('title_link',title_link.toString());
+  attachment.put('text', subject.toString());
+  attachment.put('fallback', "fallback message");
+  attachment.put('color',colorCode);
+  attachment.put('mrkdwn_in', ["fields"])
+  // JSONObject for branch
+  JSONObject branch = new JSONObject();
+  branch.put('title', 'Branch');
+  branch.put('value', branchName.toString());
+  branch.put('short', true);
+  // JSONObject for author
+  JSONObject commitAuthor = new JSONObject();
+  commitAuthor.put('title', 'Author');
+  commitAuthor.put('value', author.toString());
+  commitAuthor.put('short', true);
+  // JSONObject for branch
+  JSONObject commitMessage = new JSONObject();
+  commitMessage.put('title', 'Commit Message');
+  commitMessage.put('value', message.toString());
+  commitMessage.put('short', false);
+  // JSONObject for test results
+  JSONObject testResults = new JSONObject();
+  testResults.put('title', 'Test Summary')
+  testResults.put('value', testSummary.toString())
+  testResults.put('short', false)
+    // JSONObject for failed results
+  JSONObject failedResults = new JSONObject();
+  testResults.put('title', 'Failed Tests')
+  testResults.put('value', failedSummary.toString())
+  testResults.put('short', false)
+  attachment.put('fields', [branch, commitAuthor, commitMessage, testResults, failedResults]);
+  JSONArray attachments = new JSONArray();
+  attachments.add(attachment);
+  println attachments.toString()
+
+  // Send notifications
+  slackSend (color: colorCode, message: subject, attachments: attachments.toString(), channel: channel)
+
+}
